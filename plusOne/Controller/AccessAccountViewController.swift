4af2851d944f29be449249bea7c056d4ee2d76e1
Mona Zheng <mona.a.zheng@gmail.com>
2021-06-11 13:53:55 -0400

    changed status bar to dark style renamed view controllers to first choice and access account added positioning for access account view controller connected using segues

---
+++ plusOne/Controller/AccessAccountViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AccessAccountViewController.swift
+//  plusOne
+//
+//  Created by mona zheng on 6/10/21.
+//
+
+import UIKit
+
+class AccessAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
