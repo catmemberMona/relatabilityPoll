c3af0dce8e824a0b24889a1f4863e4bfa03045b4
Mona Zheng <mona.a.zheng@gmail.com>
2021-06-11 18:44:10 -0400

    added UI to single feed view added logo label to first choice page

---
+++ plusOne/Controller/Single Feed/SingleFeedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SingleFeedViewController.swift
+//  plusOne
+//
+//  Created by mona zheng on 6/11/21.
+//
+
+import UIKit
+
+class SingleFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ plusOne/Controller/Single Feed/SingleTVCell.swift
@@ -0,0 +1,23 @@
+//
+//  SingleTVCell.swift
+//  plusOne
+//
+//  Created by mona zheng on 6/11/21.
+//
+
+import UIKit
+
+class SingleTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
