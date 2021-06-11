b96c1897a368c93fcb85d6a276cfd082c5922c52
Mona Zheng <mona.a.zheng@gmail.com>
2021-06-11 18:44:34 -0400

    added animation to logo label

--- plusOne/Controller/FirstChoiceViewController.swift
+++ plusOne/Controller/FirstChoiceViewController.swift
@@ -9,10 +9,28 @@ import UIKit
 
 class FirstChoiceViewController: UIViewController {
 
+    @IBOutlet weak var logoLabel: UILabel!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        plusOneAnimation()
+       
+        
+    }
+    
+    func plusOneAnimation(){
+        logoLabel.text = ""
+        let logoText = "+1"
+        var charIndex = 1.0
+        for char in logoText {
+            Timer.scheduledTimer(withTimeInterval: 0.5 * charIndex, repeats: false) {
+                (timer) in
+                self.logoLabel.text?.append(char)
+            }
+            charIndex+=1
+        }
     }
     
 
