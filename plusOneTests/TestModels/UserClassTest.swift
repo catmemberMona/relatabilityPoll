8e6bff7a7ed3ff1d25350a0a30babe35355cdba5
Mona Zheng <mona.a.zheng@gmail.com>
2021-07-14 01:26:09 -0400

    added unit test for setting/adding values for User properties

--- plusOneTests/TestModels/UserClassTest.swift
+++ plusOneTests/TestModels/UserClassTest.swift
@@ -9,16 +9,16 @@ import XCTest
 @testable import plusOne
 
 class UserClassTest: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    // MARK: Inital values
+    // MARK: Properties Present
     func testInit_UserWithEmail(){
         User.email = ""
         XCTAssertNotNil(User.email)
@@ -32,5 +32,22 @@ class UserClassTest: XCTestCase {
     func testInit_UserWithReactedToPollsArray(){
         XCTAssertNotNil(User.reacted)
     }
-
+    
+    // MARK: Sets/add Values
+    func testValue_SetEmail(){
+        User.email = "test@1.com"
+        XCTAssertEqual(User.email, "test@1.com")
+    }
+    
+    func testValue_SetTokenId(){
+        User.tokenId = "some token id"
+        XCTAssertEqual(User.tokenId, "some token id")
+    }
+    
+    func testArray_AppendReactedToPollsArrayWithDictionary_ReturnCountOne(){
+        User.reacted.append(["id": 0, "userChoice": UserChoice.relatable.rawValue])
+        XCTAssertEqual(User.reacted.count, 1)
+        
+    }
+    
 }
